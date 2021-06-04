a62e024b4ef741b2fb8129b2ebfa40fe2bef0654
Tygrikon <sasha-novak@yandex.ru>
2021-06-04 11:59:09 +0300

    alarmer implementation

--- internal/alarmer/alarmer.go
+++ internal/alarmer/alarmer.go
@@ -1,27 +1,56 @@
 package alarmer
 
+import (
+	"fmt"
+	"time"
+)
+
 type Alarmer interface {
-	Alarm()
+	Alarm() <-chan struct{}
 	Init()
 	Close()
 }
 
 type alarmer struct {
-
+	timeout time.Duration
+	alarms  chan struct{}
+	done    chan struct{}
 }
 
-func New() Alarmer {
-	return &alarmer{}
+func New(timeout time.Duration) Alarmer {
+	alarms := make(chan struct{})
+	done := make(chan struct{})
+	return &alarmer{
+		timeout: timeout,
+		alarms: alarms,
+		done: done,
+	}
 }
 
-func (a * alarmer) Alarm() {
-
+func (a * alarmer) Alarm() <-chan struct{} {
+	return a.alarms
 }
 
 func (a * alarmer) Init() {
+	go func () {
+		timer := time.NewTicker(a.timeout)
+		defer timer.Stop()
+		for {
+			select {
+				case <- timer.C:
+					fmt.Println("Tick")
+					a.alarms <- struct{}{}
+				case <-a.done:
+					close(a.alarms)
+					close(a.done)
+					fmt.Println("close alarmer")
+					return
 
+			}
+		}
+	} ()
 }
 
 func (a * alarmer) Close() {
-
+	<-a.done
 }
